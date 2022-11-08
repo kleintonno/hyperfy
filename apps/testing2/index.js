13aa225367ffde297d874fbfd991609af8dc0563
FortyMcNiner <fortymcninerface@gmail.com>
2022-11-08 06:25:19 -0500

    Demon walks and teleports on enter.

--- apps/testing2/index.js
+++ apps/testing2/index.js
@@ -3,8 +3,8 @@ import { DEG2RAD, useWorld, useFields, useSyncState } from 'hyperfy'
 
 import { Tween } from './Tween'
 const anim = new Tween({ z: -8.5 })
-  .to({ z: -20 }, 4, Tween.QUAD_IN_OUT)
-  .to({ z: -8.5 }, 4, Tween.QUAD_IN_OUT)
+  .to({ z: -20 }, 5, Tween.QUAD_IN_OUT)
+  .to({ z: -8.5 }, 5, Tween.QUAD_IN_OUT)
   .loop()
 
 const anim2 = new Tween({ z: -20 })
@@ -52,6 +52,7 @@ export default function World() {
   const bodyRef5 = useRef()
   const engine = useWorld()
   const world = useWorld()
+  const [mineActive, setMineActive] = useState(false)
 
   {
     /*}
@@ -69,89 +70,24 @@ export default function World() {
       body.setPositionZ(anim.value.z)
     })
   }, [])
-  useEffect(() => {
-    const body = bodyRef2.current
-    return engine.onUpdate(() => {
-      anim2.set(engine.getServerTime())
-      body.setPositionZ(anim2.value.z)
-    })
-  }, [])
-  useEffect(() => {
-    const body = bodyRef3.current
-    return engine.onUpdate(() => {
-      anim3.set(engine.getServerTime())
-      body.setPositionZ(anim3.value.z)
-    })
-  }, [])
-  useEffect(() => {
-    const body = bodyRef4.current
-    return engine.onUpdate(() => {
-      anim4.set(engine.getServerTime() + 1)
-      body.setPositionZ(anim4.value.z)
-    })
-  }, [])
 
-  {
-    /*Launcher*/
-  }
-  useEffect(() => {
-    const body = bodyRef5.current
-    return engine.onUpdate(() => {
-      anim5.set(engine.getServerTime())
-      {
-        /*body.setPositionY(anim5.value.y)*/
-      }
-      body.setPositionZ(anim5.value.z)
-    })
-  }, [])
+  let animation = mineActive ? 'Walking' : 'Walking'
 
   return (
     <app>
       {/* elevator */}
-      <rigidbody ref={bodyRef} type="kinematic" position={[-9, 0.5, 0]}>
-        <model src="launcher.glb" scale={0.4} allColliders="trimesh" />
-      </rigidbody>
 
-      <rigidbody ref={bodyRef2} type="kinematic" position={[-4.5, 32, 0]}>
-        <model src="launcher.glb" scale={0.4} allColliders="trimesh" />
-      </rigidbody>
+      {
+        <group position={[-9, 0, 0]} ref={bodyRef}>
+          <model src="DemonWalking.glb" scale={1} animate={animation} />
 
-      <rigidbody ref={bodyRef3} type="kinematic" position={[0, 32, 0]}>
-        <model src="launcher.glb" scale={0.4} allColliders="trimesh" />
-      </rigidbody>
-
-      <rigidbody ref={bodyRef4} type="kinematic" position={[4.5, 32, 0]}>
-        <model src="launcher.glb" scale={0.4} allColliders="trimesh" />
-      </rigidbody>
-
-      {/*Launcher GLB*/}
-      <rigidbody ref={bodyRef5} type="kinematic">
-        <model
-          src="launcher.glb"
-          position={[-97.33, 59, 0]}
-          scale={0.3}
-          allColliders="trimesh"
-        />
-        <model
-          src="launcher.glb"
-          position={[-88.33, 57.15, 0]}
-          scale={0.3}
-          allColliders="trimesh"
-        />
-        <model
-          src="launcher.glb"
-          position={[-79.33, 55.15, 0]}
-          scale={0.3}
-          allColliders="trimesh"
-        />
-      </rigidbody>
-
-      <trigger
-        position={[-10, 0, -10]}
-        size={[5, 5, 5]}
-        onEnter={() => world.teleport(null, 'ghost-death')}
-        debug
-      />
+          <trigger
+            size={[2, 4, 2]}
+            onEnter={() => world.teleport(null, 'ghost-death')}
+            debug
+          />
+        </group>
+      }
 
       <place label="ghost-death" position={[0, 0, 0]} rotationY={-90} />
 
