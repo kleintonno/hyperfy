542b254a89cec2162573ea85a204779440a82211
FortyMcNiner <fortymcninerface@gmail.com>
2022-12-09 14:33:38 -0500

    New castle model and index.js. 2 quests broken.

--- apps/haunted-castle/index.js
+++ apps/haunted-castle/index.js
@@ -3,13 +3,13 @@ import { DEG2RAD, useWorld, useFields, useSyncState } from 'hyperfy'
 import { Dialog } from './Dialog'
 
 import { Tween } from './Tween'
-const anim = new Tween({ z: -30 })
+const anim = new Tween({ z: -30 }) //demon
   .to({ z: -17 }, 7, Tween.QUAD_IN_OUT)
   .to({ z: -30 }, 7, Tween.QUAD_IN_OUT)
   .loop()
 
-const anim2 = new Tween({ z: -33 })
-  .to({ z: -16 }, 4, Tween.QUAD_IN_OUT)
+const anim2 = new Tween({ x: 39 }) //ghost
+  .to({ x: 23 }, 4, Tween.QUAD_IN_OUT)
   .wait(1.5)
   .loop()
 
@@ -31,6 +31,11 @@ const schema = {
     },
   },
 }
+const schema2 = {
+  id: 'Quest2',
+  origin: 'test',
+  text: `I wouldn't go down there if I was you.`,
+}
 
 export default function World() {
   const bodyRef = useRef()
@@ -39,6 +44,8 @@ export default function World() {
   const world = useWorld()
   const [mineActive, setMineActive] = useState(false)
   const [view, setView] = useState(false)
+  const [view2, setView2] = useState(false)
+
   {
     /*}
   function safetyNet1() {
@@ -59,7 +66,7 @@ export default function World() {
     const body = bodyRef2.current
     return engine.onUpdate(() => {
       anim2.set(engine.getServerTime())
-      body.setPositionZ(anim2.value.z)
+      body.setPositionX(anim2.value.x)
     })
   }, [])
 
@@ -70,10 +77,9 @@ export default function World() {
   function death() {
     setMineActive(true)
     const name = world.getAvatar().name
-    setTimeout(() => world.teleport(null, 'haunted-respawn'), 1000)
+    setTimeout(() => world.teleport(null, 'haunted-maze'), 1000)
     setTimeout(() => setMineActive(false), 1100)
     setTimeout(() => world.chat(`${name} has been eviscerated.`), 1100)
-    origin = 'youdied'
   }
 
   function ghosted() {
@@ -95,47 +101,61 @@ export default function World() {
         </group>
       }
       {
-        <group position={[17.5, 10.5, 0]} ref={bodyRef2}>
-          <model src="ghost.glb" />
+        <group position={[0, 13, -22]} ref={bodyRef2}>
+          <model src="ghost.glb" rotation={[0, -90, 0]} />
           <trigger
             size={[2, 4, 2]}
-            position={[0, 1, 0.5]}
+            position={[-0.5, 1, 0]}
             onEnter={() => ghosted()}
           />
         </group>
       }
 
-      {/*
-        <group position={[10, 0, 10]}>
-          <model src="DemonIdle.glb" animate={animation2} />
-          <trigger
-            size={[2, 4, 2]}
-            position={[0, 1, 1.5]}
-            onEnter={() => death()}
-          />
-        </group>
-    */}
+      <place
+        label="haunted-respawn"
+        position={[-11.5, 15, -24]}
+        rotationY={-50}
+      />
+      <place
+        label="haunted-admin"
+        position={[-36.5, 18.6, -3.3]}
+        rotationY={5}
+      />
+      <place label="haunted-maze" position={[5, 2.5, -5]} rotationY={5} />
 
-      <place label="haunted-respawn" position={[0, 0, 4]} rotationY={30} />
+      <Dialog schema={schema} onView={setView} position={[3, 0, 0]}></Dialog>
 
-      {/*  47733c floor 
-      <rigidbody>
-        <box size={[420, 0.1, 420]} color="#47733c" /> 
-      </rigidbody> */}
+      <Dialog
+        schema={schema2}
+        onView={setView2}
+        position={[1, 13.8, -6]}
+      ></Dialog>
 
-      <Dialog schema={schema} onView={setView}></Dialog>
       <model src="spearman.glb" />
-      <model src="hauntedcastle_passable.glb" />
-      <model src="DemonIdle.glb" animate={animation3} />
+      <model
+        src="DemonIdle.glb"
+        position={[29, 2.5, -23.5]}
+        rotation={[0, -45, 0]}
+        animate={animation3}
+      />
       <trigger
         size={[3, 4, 3]}
-        position={[-21, 1.5, -7.5]}
+        position={[29, 3.5, -23.5]}
         onEnter={() => death()}
         debug
       />
 
       <rigidbody>
-        <model src="hauntedcastle.glb" allColliders="trimesh" />
+        <model
+          src="testcastle.glb"
+          allColliders="trimesh"
+          position={[0, 15, -0]}
+        />
+        <model
+          src="spearman.glb"
+          position={[1, 12.8, -6]}
+          rotation={[0, 210, 0]}
+        />
       </rigidbody>
     </app>
   )
